88f88f9cadeca8da1293e6d61f5a25da252d5899
Nguyễn Văn Khánh <donkihote2305@gmail.com>
2018-02-26 11:19:02 +0700

    update26.2.2018

--- app/build/generated/source/apt/debug/android/databinding/DataBindingComponent.java
+++
@@ -1,5 +0,0 @@
-package android.databinding;
-
-public interface DataBindingComponent {
-    com.mvvm.kien2111.mvvmapplication.binding.FragmentBindingAdapter getFragmentBindingAdapter();
-}
--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="575e398b-beb3-48dc-ac6c-56255e3a97ea")
-public class DataBindingInfo {}
